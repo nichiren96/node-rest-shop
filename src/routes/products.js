bcc27d1d151360f621fa429e2838ccbd770b17c8
Roel Tombozafy <rtombozafy96@gmail.com>
2021-07-29 18:47:08 +0300

    Added crud operations on orders

--- src/routes/products.js
+++ src/routes/products.js
@@ -6,11 +6,25 @@ const Product = require('../models/product')
 
 router.get('/', (req, res, next) => {
    Product.find()
+        .select('name price _id')
         .exec()
         .then(docs => {
-            console.log(docs)
             if(docs.length > 0) {
-                res.status(200).json(docs)
+                const response = {
+                    count: docs.length,
+                    products: docs.map(doc => {
+                        return {
+                            id: doc._id,
+                            name: doc.name,
+                            price: doc.price,
+                            request: {
+                                type: 'GET',
+                                url: 'http://localhost:9201/products/'+doc._id
+                            }
+                        }
+                    })
+                } 
+                res.status(200).json(response)
             }else {
                  res.status(404).json({message: 'No entries found'})
             }
@@ -34,8 +48,16 @@ router.post('/', (req, res, next) => {
         .then(result => {
             console.log(result)
             res.status(201).json({
-                message: 'Handling POST requests to /products',
-                createdProduct: result
+                message: 'Created product successfully',
+                createdProduct: {
+                    id: result._id,
+                    name: result.name,
+                    price: result.price,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:9201/products/'+result._id
+                    }
+                }
             })
         })
         .catch(err => {
@@ -48,11 +70,17 @@ router.post('/', (req, res, next) => {
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId
     Product.findById(id)
+        .select('name price _id')
         .exec()
         .then(doc => {
-            console.log(doc)
             if(doc) {
-                res.status(200).json(doc)
+                res.status(200).json({
+                    product: { id: doc._id, name: doc.name, price: doc.price },
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:9201/products'
+                    }
+                })
             }else {
                 res.status(404).json({message: 'No entry found for the provided ID'})
             }
@@ -71,14 +99,19 @@ router.patch('/:productId', (req, res, next) => {
     for(const ops of req.body) {
         updateOps[ops.propName] = ops.value
     }
-    console.table(updateOps)
-
+   
     Product.findByIdAndUpdate(id, updateOps, (err, doc)=>{
         if(err){
             console.log(err)
             res.status(500).json({error: err})
         }else {
-            res.status(200).json(doc)
+            res.status(200).json({
+                message: 'Product updated',
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:9201/products/' + id
+                }
+            })
         }
     })
 })
@@ -87,7 +120,14 @@ router.delete('/:productId', (req, res, next) => {
     const id = req.params.productId
     Product.remove({_id: id})
         .then(result => {
-             res.status(200).json(result)
+             res.status(200).json({
+                message: 'Product deleted',
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:9201/products',
+                    body: { name: 'String', price: 'Number' }
+                }
+             })
         })
         .catch(err => {
             res.status(500).json({error: err})
