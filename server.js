5ac3d4d9a39d122b53fbc6f1ebbbe7f0a4ba1f67
Roel Tombozafy <rtombozafy96@gmail.com>
2022-05-19 14:34:44 +0300

    Changed default port to 8000

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const http = require('http')
-const dotenv = require('dotenv')
+const http = require("http");
+const dotenv = require("dotenv");
 
-dotenv.config({path: './.env'})
+dotenv.config({ path: "./.env" });
 
-const app = require('./src/app')
-const port = process.env.PORT || 3000
+const app = require("./src/app");
+const port = process.env.PORT || 8000;
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-server.listen(port, () => console.log(`Server listening on port ${port}`))
+server.listen(port, () => console.log(`Server listening on port ${port}`));
