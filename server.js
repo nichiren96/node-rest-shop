85e7bf7262955ca34c7de79e55d6a03c78f5783f
Roel Tombozafy <rtombozafy96@gmail.com>
2022-05-19 14:55:12 +0300

    Changed server response message

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ const port = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(port, () => console.log(`Server listening on port ${port}`));
+server.listen(port, () => console.log(`Server is listening on port ${port}`));
